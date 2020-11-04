d3da00c06bf93a5665224391857656dbc1453148
Fábio Pichler <fabiopichler1992@gmail.com>
2020-11-04 20:38:27 -0300

    rename LuaBase to LuaUtil

--- include/OmicronTK/lua/util/LuaUtil.hpp
+++ include/OmicronTK/lua/util/LuaUtil.hpp
@@ -27,10 +27,50 @@
 
 *******************************************************************************/
 
-#include "OmicronTK/lua/LuaBase.hpp"
+#pragma once
+
+#include "OmicronTK/lua/global.h"
+
+#include <lua.hpp>
+#include <vector>
 
 namespace OmicronTK {
 namespace lua {
+
+class OTKLUA_EXPORT LuaUtil
+{
+public:
+    template<typename LuaClass>
+    static inline void newUserData(lua_State *L, const char *tableName, void *userdata)
+    {
+        *static_cast<void **>(lua_newuserdata(L, sizeof(LuaClass *))) = userdata;
+        luaL_setmetatable(L, tableName);
+    }
+
+    template<typename LuaClass>
+    static inline LuaClass *checkUserData(lua_State *L, int ud, const char *tableName)
+    {
+        return *static_cast<LuaClass **>(luaL_checkudata(L, ud, tableName));
+    }
+
+    template<typename LuaClass>
+    static inline int __gc(lua_State *L, const char *tableName)
+    {
+        LuaClass *userdata = checkUserData<LuaClass>(L, 1, tableName);
+
+        if (userdata)
+        {
+            delete userdata;
+        }
+        else
+        {
+            lua_pushstring(L, "C++ object garbage failure");
+            lua_error(L);
+        }
+
+        return 0;
+    }
+};
 
 }
 }
