358e99f522f1fb3fd933626eac2821ca18aa3de9
Fábio Pichler <fabiopichler1992@gmail.com>
2020-11-25 00:28:40 -0300

    update LuaClass

--- include/OmicronTK/lua/LuaClass.hpp
+++ include/OmicronTK/lua/LuaClass.hpp
@@ -14,6 +14,21 @@ public:
     inline void addMember(const LuaReg &reg) { m_members.push_back(reg); }
     inline void addMetamethod(const LuaReg &reg) { m_metamethods.push_back(reg); }
 
+    inline void addStatic(const std::string &field, const LuaValue &value)
+    {
+        m_statics.push_back({field.c_str(), value});
+    }
+
+    inline void addMember(const std::string &field, const LuaValue &value)
+    {
+        m_members.push_back({field.c_str(), value});
+    }
+
+    inline void addMetamethod(const std::string &field, const LuaValue &value)
+    {
+        m_metamethods.push_back({field.c_str(), value});
+    }
+
     inline void addConstructor(const LuaValue &constructor) { m_members.push_back({ "constructor", constructor }); }
     inline void addDestructor(const LuaValue &__gc) { m_metamethods.push_back({ "__gc", __gc }); }
 
