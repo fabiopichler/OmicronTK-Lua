51ed6756fda762e969fc66485ec8f77fbca053cf
Fábio Pichler <fabiopichler1992@gmail.com>
2021-02-16 09:58:17 -0300

    updated

--- include/OmicronTK/lua/CallbackInfo.hpp
+++ include/OmicronTK/lua/CallbackInfo.hpp
@@ -2,6 +2,8 @@
 
 #include "OmicronTK/lua/defines.hpp"
 
+#include <lua.hpp>
+
 namespace OmicronTK {
 namespace lua {
 
@@ -13,6 +15,30 @@ public:
     inline lua_State *state() { return m_state; }
 
     int length();
+
+    double getNumber(int idx);
+    float getFloat(int idx);
+    int getInteger(int idx);
+    unsigned int getUInt(int idx);
+    long getLong(int idx);
+    unsigned long getULong(int idx);
+    std::string getString(int idx);
+    lua_CFunction getCFunction(int idx);
+    bool getBoolean(int idx);
+    void *getUserData(int idx);
+    void *getLightUserData(int idx);
+
+    template<typename T>
+    inline T *getUserData(int idx)
+    {
+        return static_cast<T *>(getUserData(idx));
+    }
+
+    template<typename T>
+    inline T *getLightUserData(int idx)
+    {
+        return static_cast<T *>(getLightUserData(idx));
+    }
 
 private:
     lua_State *m_state;
--- src/CallbackInfo.cpp
+++ src/CallbackInfo.cpp
@@ -16,5 +16,73 @@ int CallbackInfo::length()
     return lua_gettop(m_state);
 }
 
+double CallbackInfo::getNumber(int idx)
+{
+    luaL_checktype(m_state, idx, LUA_TNUMBER);
+    return double (lua_tonumber(m_state, idx));
+}
+
+float CallbackInfo::getFloat(int idx)
+{
+    luaL_checktype(m_state, idx, LUA_TNUMBER);
+    return float (lua_tonumber(m_state, idx));
+}
+
+int CallbackInfo::getInteger(int idx)
+{
+    luaL_checktype(m_state, idx, LUA_TNUMBER);
+    return int (lua_tointeger(m_state, idx));
+}
+
+unsigned int CallbackInfo::getUInt(int idx)
+{
+    luaL_checktype(m_state, idx, LUA_TNUMBER);
+    return static_cast<unsigned int>(lua_tonumber(m_state, idx));
+}
+
+long CallbackInfo::getLong(int idx)
+{
+    luaL_checktype(m_state, idx, LUA_TNUMBER);
+    return long (lua_tonumber(m_state, idx));
+}
+
+unsigned long CallbackInfo::getULong(int idx)
+{
+    luaL_checktype(m_state, idx, LUA_TNUMBER);
+    return static_cast<unsigned long>(lua_tonumber(m_state, idx));
+}
+
+std::string CallbackInfo::getString(int idx)
+{
+    luaL_checktype(m_state, idx, LUA_TSTRING);
+    return lua_tostring(m_state, idx);
+}
+
+lua_CFunction CallbackInfo::getCFunction(int idx)
+{
+    luaL_checktype(m_state, idx, LUA_TFUNCTION);
+    return lua_tocfunction(m_state, idx);
+}
+
+bool CallbackInfo::getBoolean(int idx)
+{
+    luaL_checktype(m_state, idx, LUA_TBOOLEAN);
+    return bool (lua_toboolean(m_state, idx));
+}
+
+void *CallbackInfo::getUserData(int idx)
+{
+    lua_getfield(m_state, idx, "__userdata");
+    luaL_checktype(m_state, -1, LUA_TUSERDATA);
+
+    return *static_cast<void **>(lua_touserdata(m_state, -1));
+}
+
+void *CallbackInfo::getLightUserData(int idx)
+{
+    luaL_checktype(m_state, idx, LUA_TLIGHTUSERDATA);
+    return lua_touserdata(m_state, idx);
+}
+
 }
 }
